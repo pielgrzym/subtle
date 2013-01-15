eaff6c911a5cca07d064c8c3b07ad101ca04c62d
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2013-01-15 11:34:47 +0100

    Fixing sublet problems

--- subtle.rb
+++ subtle.rb
@@ -38,7 +38,7 @@ set :wmname, "LG3D"
 
 # Screens {{{
 screen 1 do
-  top     [:title, :spacer, :center, :views, :center, :tray, :cpu, :sublets, :separator, :clock]
+  top     [:title, :spacer, :center, :views, :center, :tray, :cpu, :separator, :clock]
   bottom  []
   view    0
 end
